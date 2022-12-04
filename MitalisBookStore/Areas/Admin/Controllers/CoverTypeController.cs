2dc2376c1590a1c1a2014d7f44bc3f99c53ab1d1
MitaliKatudia <MK494@myscc.ca>
2022-12-04 14:30:48 -0500

    Updated CoverType to implement Stored Procedure

--- MitalisBookStore/Areas/Admin/Controllers/CoverTypeController.cs
+++ MitalisBookStore/Areas/Admin/Controllers/CoverTypeController.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MitalisBooks.Models;
+using MitalisBooks.Utility;
+using Dapper;
 
 namespace MitalisBookStore.Areas.Admin.Controllers
 {
@@ -40,8 +42,11 @@ namespace MitalisBookStore.Areas.Admin.Controllers
             }
 
             //this is for edit
-            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            //coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
 
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
             if (coverType == null)
             {
                 return NotFound();
@@ -58,14 +63,20 @@ namespace MitalisBookStore.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)   // check all variables in the model (e.g. Name Required) to increase security
             {
+
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", coverType.Name);
                 if (coverType.Id == 0)
                 {
-                    _unitOfWork.CoverType.Add(coverType);
+                    //_unitOfWork.CoverType.Add(coverType);
                     //_unitOfWork.save();
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter);
                 }
                 else
                 {
-                    _unitOfWork.CoverType.Update(coverType);
+                    // _unitOfWork.CoverType.Update(coverType);
+                    parameter.Add("@Id", coverType.Id);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
                 }
 
                 _unitOfWork.Save();
@@ -82,7 +93,8 @@ namespace MitalisBookStore.Areas.Admin.Controllers
         public IActionResult GetAll()
         {
             // return NotFound();
-            var allObj = _unitOfWork.CoverType.GetAll();
+            // var allObj = _unitOfWork.CoverType.GetAll();
+            var allObj = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll, null);
             return Json(new { data = allObj });
         }
 
@@ -90,12 +102,16 @@ namespace MitalisBookStore.Areas.Admin.Controllers
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.CoverType.Get(id);
+            //var objFromDb = _unitOfWork.CoverType.Get(id);
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            var objFromDb = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
             if (objFromDb == null)
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
-            _unitOfWork.CoverType.Remove(objFromDb);
+            //_unitOfWork.CoverType.Remove(objFromDb);
+            _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameter);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Delete Successfully" });
         }
